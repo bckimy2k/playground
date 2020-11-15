66191cde27c4c2519aa6e34c1579889a21aa2c29
Byung Kim <byung.kim@motorolasolutions.com>
2020-11-14 16:23:23 -0800

    directive - replace/restrict test

--- myAppController.js
+++ myAppController.js
@@ -1,13 +1,8 @@
 angular.module('myApp', [])
-.controller('messageController', function($scope) {
-     $scope.getMessage = function() {
-         setTimeout(function() {
-              // wrapped this within $apply
-              $scope.$applyAsync(function() {
-                $scope.message = 'fetched after 3 seconds';
-              });
-              console.log('message :' + $scope.message);
-         }, 3000);
-     };
-     $scope.getMessage();
+.directive('myDirective', function() {
+    return {
+      restrict: 'EA',
+      replace : true,
+      template: '<a href="http://google.com"> Click me to go to Google </a>'
+    };
   });
